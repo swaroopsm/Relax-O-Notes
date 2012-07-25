88072f24226dacd1802c414cd19a342f75fbb8d5
Swaroop SM <swaroop.striker@gmail.com>
2012-07-25 17:04:23 +0530

    function for uplaod file button click to create a doc with email field

--- _attachments/js/relax.js
+++ _attachments/js/relax.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
 	$db="relax-o-notes";
+	
+	$("#upload_file_btn").live("click", function(){
+		var doc={
+			"uploaded_by": $("#uploaded_by").val(),
+		};
+		$.couch.db($db).saveDoc(doc,{
+			success: function(data){
+				var id=data.id;
+				var stat=data.ok;
+				if(stat){
+					$("#uploadModal").modal('hide');
+					
+				}
+				console.log(data);
+			},
+			error: function(data){
+				console.log(data);
+			}
+		});
+		console.log($db);
+		return false;
+	});
+	
 });
