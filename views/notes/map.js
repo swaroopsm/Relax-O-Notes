5abdea7c13bf2be4f48cae5bbad67b41c2c5aef7
Swaroop SM <swaroop.striker@gmail.com>
2012-07-26 09:56:17 +0530

    sort based on twitter handle

--- views/notes/map.js
+++ views/notes/map.js
@@ -1,5 +1,5 @@
 function(doc){
 	if(doc.uploaded_by){
-		emit(doc._id,{"user": doc.uploaded_by,"avatar": doc.gravatar_url,"uploader_msg": doc.uploader_msg,"created_at":doc.created_at,"tags":doc.tags});
+		emit(doc.uploaded_by,{"user": doc.uploaded_by,"avatar": doc.gravatar_url,"uploader_msg": doc.uploader_msg,"created_at":doc.created_at,"tags":doc.tags});
 	}
 }
