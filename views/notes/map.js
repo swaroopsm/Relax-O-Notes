6f66f75357f0b5639d715867bab8011b898343e4
Swaroop SM <swaroop.striker@gmail.com>
2012-07-31 11:22:56 +0530

    fixed a bug that was encountered in sorting

--- views/notes/map.js
+++ views/notes/map.js
@@ -1,7 +1,9 @@
 function(doc){
 	if(doc.uploaded_by){
-		var d=new Date();
-		var t=d-Date.parse(doc.created_at);
-		emit(t,{"user": doc.uploaded_by,"avatar": doc.gravatar_url,"uploader_msg": doc.uploader_msg,"created_at":doc.created_at,"tags":doc.tags});
+		if(doc.created_at){
+			var d=new Date();
+			var t=d-Date.parse(doc.created_at);
+			emit(t,{"user": doc.uploaded_by,"avatar": doc.gravatar_url,"uploader_msg": doc.uploader_msg,"created_at":doc.created_at,"tags":doc.tags});
+		}
 	}
 }
