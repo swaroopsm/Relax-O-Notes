1e163bc87c8aec07a7df62967efcbae1f09dec1b
Swaroop SM <swaroop.striker@gmail.com>
2012-08-01 00:31:48 +0530

    sorting according to timestamps

--- views/discuss/map.js
+++ views/discuss/map.js
@@ -1,7 +1,9 @@
 function(doc){
 	if(doc.d_title){
-		var d=new Date();
-		var t=d-Date.parse(doc.d_date);
-		emit(t,{"title": doc.d_title, "author": doc.d_author, "message": doc.d_msg,"avatar": doc.author_pic,"comments": doc.d_comments, "created_at": doc.d_date});
+		if(doc.d_date){
+			var d=new Date();
+			var t=d-Date.parse(doc.d_date);
+			emit(t,{"title": doc.d_title, "author": doc.d_author, "message": doc.d_msg,"avatar": doc.author_pic,"comments": doc.d_comments, "created_at": doc.d_date});
+		}
 	}
 }
