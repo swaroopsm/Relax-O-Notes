783598a6cce01aabd56b0a838358ae8f721de9bd
Swaroop SM <swaroop.striker@gmail.com>
2012-07-31 23:31:00 +0530

    function to emit discussions

---
+++ views/discuss/map.js
@@ -0,0 +1,7 @@
+function(doc){
+	if(doc.d_title){
+		var d=new Date();
+		var t=d-Date.parse(doc.d_date);
+		emit(t,{"title": doc.d_title, "author": doc.d_author, "message": doc.d_msg,"avatar": doc.author_pic,"comments": doc.d_comments, "created_at": doc.d_date});
+	}
+}
