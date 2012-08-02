a8dc8ef53337a94f93fd9b49ab841f9a78fe699d
Swaroop SM <swaroop.striker@gmail.com>
2012-08-02 17:01:31 +0530

    removed if(doc.uploaded_by)) condition

--- views/notes/map.js
+++ views/notes/map.js
@@ -1,9 +1,7 @@
 function(doc){
-	if(doc.uploaded_by){
 		if(doc.created_at){
 			var d=new Date();
 			var t=d-Date.parse(doc.created_at);
 			emit(t,{"user": doc.uploaded_by,"avatar": doc.gravatar_url,"uploader_msg": doc.uploader_msg,"created_at":doc.created_at,"tags":doc.tags});
 		}
-	}
 }
