7bc2e8ef5f29ae88cfa83a2c1ae73bc6b5056ea2
Swaroop SM <swaroop.striker@gmail.com>
2012-08-08 21:27:01 +0530

    change in field names

--- views/discuss/map.js
+++ views/discuss/map.js
@@ -1,7 +1,7 @@
 function(doc){
-		if(doc.d_date){
+		if(doc.type=='discussion'){
 			var d=new Date();
-			var t=d-Date.parse(doc.d_date);
-			emit(t,{"title": doc.d_title, "author": doc.d_author, "message": doc.d_msg,"avatar": doc.author_pic,"comments": doc.d_comments, "created_at": doc.d_date});
+			var t=d-Date.parse(doc.date);
+			emit(t,{"title": doc.title, "author": doc.author, "content": doc.content,"avatar": doc.author_pic, "created_at": doc.date});
 		}
 }
