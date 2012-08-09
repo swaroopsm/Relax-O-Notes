dbfc2340f4d9a213271405b36856c7e2bb68d6a1
Swaroop SM <swaroop.striker@gmail.com>
2012-08-09 22:10:37 +0530

    emit date of the comment also

--- views/comments/map.js
+++ views/comments/map.js
@@ -1,5 +1,5 @@
 function(doc){
 	if(doc.type==='comment'){
-		emit(doc.discussion,{"author":doc.author,"comment":doc.comment,"avatar":doc.author_pic});
+		emit(doc.discussion,{"author":doc.author,"comment":doc.comment,"avatar":doc.author_pic,"created_at":doc.date});
 	}
 }
