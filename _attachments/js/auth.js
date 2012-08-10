89b64a2d6a743a0271634d4fc002caa338262823
Swaroop SM <swaroop.striker@gmail.com>
2012-08-10 19:38:19 +0530

    initial twitter auth contents

--- _attachments/js/auth.js
+++ _attachments/js/auth.js
@@ -1,1 +1,13 @@
-var api_key='';
+twttr.anywhere(function (T) {
+	if(T.isConnected()){
+		console.log("Connected!");
+	}
+  $("#test").click(function(){
+  	if(T.isConnected()){
+  		console.log("Connected!");
+  	}
+  	else{
+  		T.signIn();
+  	}
+  });
+});
