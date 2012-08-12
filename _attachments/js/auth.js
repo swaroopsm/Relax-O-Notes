558e0eee15766a736b6e44cecab1edf98b738703
Swaroop SM <swaroop.striker@gmail.com>
2012-08-12 23:38:28 +0530

    get logged in user pic and dsiplay it

--- _attachments/js/auth.js
+++ _attachments/js/auth.js
@@ -1,6 +1,10 @@
 twttr.anywhere(function (T) {
 	if(T.isConnected()){
-		$("#peepin").html('<a data-toggle="modal" href="#uploadModal" class="btn btn-success">Tweet &raquo;</a>').hide().fadeIn('fast');
+		var user=T.currentUser;
+		var p=user.data('profile_image_url');
+		var a=p.split('_');
+		p=a[0]+"_"+a[1]+"_"+"mini.jpeg";
+		$("#peepin").html('<li id="fat-menu" class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="'+p+'" ></img> '+user.data("screen_name")+'<b class="caret"></b></a><ul class="dropdown-menu"><li><a href="index.html">Home<a></li><li><a href="discussion.html">Discussions</a></li><li><a href="files.html">Files</a></li><li class="divider"></li><li><a href="#">Log out!</a></li></ul></li>').hide().fadeIn('fast');
 	}
 	else{
 		$("#peepin").html('<a id="login" class="btn btn-success">Login &raquo;</a>').show();
