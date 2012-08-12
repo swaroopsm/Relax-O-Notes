e2d67c69eed88267d00bdb17d6ea34ba32c2334e
Swaroop SM <swaroop.striker@gmail.com>
2012-08-12 22:52:34 +0530

    check if user logged in and if yes the tweet button or else the login btn

--- _attachments/js/auth.js
+++ _attachments/js/auth.js
@@ -1,10 +1,13 @@
 twttr.anywhere(function (T) {
 	if(T.isConnected()){
-		console.log("Connected!");
+		$("#peepin").html('<a data-toggle="modal" href="#uploadModal" class="btn btn-success">Tweet &raquo;</a>').hide().fadeIn('fast');
 	}
-  $("#test").click(function(){
+	else{
+		$("#peepin").html('<a id="login" class="btn btn-success">Login &raquo;</a>').show();
+	}
+  $("#login").live("click",function(){
   	if(T.isConnected()){
-  		console.log("Connected!");
+  		$("#peepin").html('<a data-toggle="modal" href="#uploadModal" class="btn btn-success">Tweet &raquo;</a>').hide().fadeIn(500);
   	}
   	else{
   		T.signIn();
