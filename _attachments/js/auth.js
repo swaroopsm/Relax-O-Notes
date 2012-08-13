5e6f9668b57976207f36148239b5960e1e4e0c6f
Swaroop SM <swaroop.striker@gmail.com>
2012-08-13 16:50:50 +0530

    added logout function

--- _attachments/js/auth.js
+++ _attachments/js/auth.js
@@ -4,7 +4,7 @@ twttr.anywhere(function (T) {
 		var p=user.data('profile_image_url');
 		var a=p.split('_');
 		p=a[0]+"_"+a[1]+"_"+"mini.jpeg";
-		$("#peepin").html('<li id="fat-menu" class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="'+p+'" ></img> '+user.data("screen_name")+'<b class="caret"></b></a><ul class="dropdown-menu"><li><a href="index.html">Home<a></li><li><a href="discussion.html">Discussions</a></li><li><a href="files.html">Files</a></li><li class="divider"></li><li><a href="#">Log out!</a></li></ul></li>').hide().fadeIn('fast');
+		$("#peepin").html('<li id="fat-menu" class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="'+p+'" ></img> '+user.data("screen_name")+'<b class="caret"></b></a><ul class="dropdown-menu"><li><a href="index.html">Home<a></li><li><a href="discussion.html">Discussions</a></li><li><a href="files.html">Files</a></li><li class="divider"></li><li><a href="#" id="logout">Log out!</a></li></ul></li>').hide().fadeIn('fast');
 	}
 	else{
 		$("#peepin").html('<a id="login" class="btn btn-success">Login &raquo;</a>').show();
@@ -16,5 +16,9 @@ twttr.anywhere(function (T) {
   	else{
   		T.signIn();
   	}
+  });
+  $("#logout").live("click",function(){
+  	twttr.anywhere.signOut();
+  	window.location="index.html";
   });
 });
