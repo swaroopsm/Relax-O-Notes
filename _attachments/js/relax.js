8c34dadad64b3f71f3306479df3e5d5c5b21df6b
Swaroop SM <swaroop.striker@gmail.com>
2012-08-15 12:00:53 +0530

    removed function for submitting tweet and updated change function to detect if it is a tweet

--- _attachments/js/relax.js
+++ _attachments/js/relax.js
@@ -2,45 +2,10 @@ $(document).ready(function(){
 	$db="relax-o-notes";
 	$url="http://localhost:5984/"+$db+"/_design/app/";
 	$flag=0;
-	
+
 	var p=$("#page_name").attr("data-page");
 	
 	if(p=="index.html"){
-		$("#upload_file_btn").live("click", function(){
-		var d=new Date();
-		d=d.toISOString();
-		var tags=$("#upload_message").val().split(" ");
-		var act_tags="", k=0;
-		for(var j=0;j<tags.length;j++){
-			if(tags[j].indexOf('#') == 0){
-				act_tags=act_tags+" "+tags[j];
-				act_tags=act_tags.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-				k++;
-			}
-		}
-		act_tags=act_tags.substring(1,act_tags.length);
-		var doc={
-			"uploaded_by": $("#uploaded_by").val(),
-			"gravatar_url": "http://api.twitter.com/1/users/profile_image/"+$("#uploaded_by").val(),
-			"uploader_msg": $("#upload_message").val(),
-			"created_at": d,
-			"tags": act_tags.split(" ")
-		};
-		$.couch.db($db).saveDoc(doc,{
-			success: function(data){
-				var id=data.id;
-				var stat=data.ok;
-				if(stat){
-					$("#uploadModal").modal('hide');
-				}
-			},
-			error: function(data){
-				console.log(data);
-			}
-		});
-		return false;
-	});
-		
 		$.couch.db($db).view("app/notes/",{
 			success: function(data){
 				$("#all_notes").html('');
@@ -190,7 +155,7 @@ $(document).ready(function(){
 				}
 			});
 		}
-		else if(sub="up_file"){
+		else if(sub=="up_file"){
 			$.couch.db($db).openDoc(id,{
 				success: function(data){
 					var abc=data._rev.split('-');
@@ -212,7 +177,7 @@ $(document).ready(function(){
 				}
 			});
 		}
-		else{
+		else if(sub=='mytweet'){
 		$.couch.db($db).openDoc(id,{
 			success: function(obj){
 				$("#all_notes").prepend("<tr id="+id+"><td class='span1'><img class='thumbnail' src='"+obj.gravatar_url+"'></img><td><a href='http://twitter.com/"+obj.uploaded_by+"' target='_BLANK'>"+obj.uploaded_by+"</a><p>"+obj.uploader_msg+"<br><span id='timeago' title='"+obj.created_at+"' class='date_time-block'></span></p></td></tr>").hide().fadeIn(500);
@@ -225,6 +190,9 @@ $(document).ready(function(){
 				}
 			}
 		});
+	 }
+	 else{
+	 	
 	 }
 	});
 	
