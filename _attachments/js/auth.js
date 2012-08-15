865d95d29a55f3208aa4d5217b4a6e47cb837fa2
Swaroop SM <swaroop.striker@gmail.com>
2012-08-15 19:58:57 +0530

    added upload_next_btn click function and attachment form submit function

--- _attachments/js/auth.js
+++ _attachments/js/auth.js
@@ -85,6 +85,85 @@ twttr.anywhere(function (T) {
 		return false;
 	});	
 	
+	$("#upload_next_btn").live("click",function(){
+		var ftitle=$.trim($("#file_title").val());
+		var fdesc=$.trim($("#file_description").val());
+		if(ftitle=='' || fdesc==''){
+			if(ftitle==''){
+				$("#file_title").focus();
+			}
+			else if(fdesc==''){
+				$("#file_description").focus();
+			}
+			else{
+					
+			}
+			return;
+		}
+		var uid=$.couch.newUUID();
+		var my_rev;
+		uid="up_file_"+uid;
+		var d=new Date();
+		d=d.toISOString();
+		var doc={
+			"_id": uid,
+			"author": user_name,
+			"title": ftitle,
+			"description": fdesc,
+			"type": "file",
+			"author_pic": "http://api.twitter.com/1/users/profile_image/"+user_name,
+			"date": d
+		};
+		$.couch.db($db).saveDoc(doc,{
+			success: function(data){
+				my_rev=data.rev;
+				$("#first_upload_body").hide();
+				$("#second_upload_body").html("<form class='form form-horizontal' id='attachment_form' name='attachment_form' content-type='multipart/form-data'><div class='control-group'><div class='controls'><input class='span' id='_attachments' name='_attachments' type='file'></div><input type='hidden' name='_id' value='"+uid+"'><input type='hidden' name='_rev' value='"+my_rev+"'><br><div class='controls'><input type='submit' class='btn btn-success' value='Upload &raquo;' id='file_btn'></div></div></form>").hide().fadeIn(500);
+				$("#upload_modal_footer").hide();
+			},
+			error: function(data){
+				console.log(data);
+			}
+		});
+	});
+	
+	$("#attachment_form").live("submit",function(e) { // invoke callback on submit
+  e.preventDefault();
+  var data = {};
+  $.each($("form :input").serializeArray(), function(i, field) {
+    data[field.name] = field.value;
+  });
+  $("form :file").each(function() {
+    data[this.name] = this.value; // file inputs need special handling
+  });
+  if (!data._attachments || data._attachments.length == 0) {
+    alert("Please select a file to upload.");
+    return;
+  }
+  $("#second_upload_body").prepend("<center><img id='loader' src='img/loader.gif'/></center>").hide().show();
+  $("#file_btn").attr("disabled","disabled");
+  $(this).ajaxSubmit({
+    url:  "../../"+data._id,
+    success: function(resp) {
+    	$("#loader").hide();
+    	$("#file_btn").attr("disabled",false);
+    	$("#uploadFileModal").modal('hide');
+    	$("#second_upload_body").hide();
+    	$("#file_title").val('');
+    	$("#file_description").val('');
+    	$("#_attachments").val('');
+    	$("#first_upload_body").show();
+    	$("#upload_modal_footer").show();
+    },
+    error: function(err){
+    	$("#loader").hide();
+    	console.log("id "+data._id);
+    	console.log(err);
+    }
+  });
+  return false;
+});
+	
 	}
 	else{
 		$("#uploadModal").on("shown",function(){
@@ -106,6 +185,9 @@ twttr.anywhere(function (T) {
 					$("#messages").html("<div class='alert alert-danger'><a class='close' data-dismiss='alert' href='#'>&times;</a><center>You need to be logged in!</center></div>").hide().fadeIn(500);
 				}
 			});
+		});
+		$("#uploadFileModal").on("shown",function(){
+			console.log("Acctive!!");
 		});
 		$("#peepin").html('<a id="login" class="btn btn-success">Login &raquo;</a>').show();
 	}
@@ -141,6 +223,6 @@ function show_dropdown(T){
 		}
 		if(page==="files.html"){
 			$("#main-menu").hide();
-			$("#upload_btn_div").html('<a data-toggle="modal" href="#uploadModal" class="btn btn-success">Upload File &raquo;</a>').hide().fadeIn(500);
+			$("#upload_btn_div").html('<a data-toggle="modal" href="#uploadFileModal" class="btn btn-success">Upload File &raquo;</a>').hide().fadeIn(500);
 		}
 }
